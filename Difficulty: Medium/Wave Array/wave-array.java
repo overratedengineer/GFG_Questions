1c1fc24ce29cdcbb1db1a24117b0edca377d55d3
Pulkit <choprapulkitt@gmail.com>
2026-04-11 11:25:53 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Wave Array/wave-array.java
@@ -0,0 +1,10 @@
+class Solution {
+    public void sortInWave(int arr[]) {
+        int n = arr.length;
+       for(int i=1;i<n;i+=2){
+           int temp = arr[i];
+           arr[i]=arr[i-1];
+           arr[i-1]=temp;
+       }
+    }
+}
