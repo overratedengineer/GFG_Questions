4420f477f733f6e509ce7138b8f9b2a31b40608d
Pulkit <choprapulkitt@gmail.com>
2026-07-17 15:44:59 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
+++ Difficulty: Medium/Count Subarrays with given XOR/count-subarrays-with-given-xor.java
@@ -1,27 +1,20 @@
 class Solution {
     public long subarrayXor(int arr[], int k) {
-      HashMap<Integer,Integer> map = new HashMap<>();
-      //I must say solve the question, it is easy after 1 calculation
-      map.put(0,1);
       int n = arr.length;
-      int xor = 0;
-      int count =0;
-      for(int i=0;i<n;i++){
-          xor^=arr[i];
-          int needed = k^xor;
-          if(map.containsKey(needed)){
-              int val = map.get(needed);
-              count+=val;
-          }
-          if(map.containsKey(xor)){
-              int v = map.get(xor);
-              map.put(xor,v+1);
-          } else {
-             map.put(xor,1);  
-          }
-          
-      }
-      return count;
-        
+     HashMap<Integer,Integer> map = new HashMap<>();
+     int x =0;
+     map.put(0,1);
+     int ans =0;
+     for(int i=0;i<n;i++){
+         x^=arr[i];
+         int needed = x^k;
+         int kl =1;
+         if(map.containsKey(needed)){
+             kl=map.get(needed);
+             ans+=kl;
+         }
+        map.put(x, map.getOrDefault(x, 0) + 1);
+     }
+        return ans;
     }
 }
