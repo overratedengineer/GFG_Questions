945db3e39a981be280d47a143451a2eeaa79fb61
Pulkit <choprapulkitt@gmail.com>
2026-07-22 23:14:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.java
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.java
@@ -1,30 +1,28 @@
 class Solution {
     public int aggressiveCows(int[] arr, int k) {
-     Arrays.sort(arr);
-     int n = arr.length;
-    
-     int s =1;
-     int e = arr[n-1]-arr[0];
-     int ans =0;
-     while(s<=e){
-         int mid = s+(e-s)/2;
-         int diff = arr[0];
-         int cows=1;
-         for(int i=1;i<n;i++){
-             if(arr[i]-diff>=mid){
-                 cows++;
-                 diff=arr[i];
-             }
-         }
-            if(cows>=k){
-                 ans =mid;
+       int n = arr.length;
+       Arrays.sort(arr);
+       int s = 0;
+       int e =arr[n-1]-arr[0];
+       int ans =0;
+       while(s<=e){
+           int mid = s+(e-s)/2;
+           int kn =1;
+           int prev = arr[0];
+           for(int i=1;i<n;i++){
+               int diff=arr[i]-prev;
+               if(diff>=mid){
+                   kn++;
+                   prev=arr[i];
+               }
+           }
+            if(kn>=k){
+                ans=mid;
                 s=mid+1;
-               
-            } else{
+            } else {
                 e=mid-1;
-               
-            } 
-     }
-     return ans;
+            }
+       }
+       return ans;
     }
 }
