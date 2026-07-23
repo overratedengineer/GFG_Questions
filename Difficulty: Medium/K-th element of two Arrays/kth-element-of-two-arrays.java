80634de70331caf4870bade640d25518d4e534e9
Pulkit <choprapulkitt@gmail.com>
2026-07-23 19:55:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -1,32 +1,29 @@
 class Solution {
-    public int kthElement(int a[], int b[], int k) {
-        int n1 = a.length;
-        int n2 = b.length;
-        if(n1>n2) return kthElement(b,a,k);
-        int n = n1+n2;
-         int s = Math.max(0, k - n2);
-        int e = Math.min(n1, k);
-        int left = k;
+    public int kthElement(int nums1[], int nums2[], int k) {
+       if(nums1.length > nums2.length)
+            return kthElement(nums2, nums1,k);
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+       int s = Math.max(0, k - n2);
+       int e = Math.min(k, n1);
         while(s<=e){
-            int mid = (s+e)/2;
-            int mid2 = left-mid;
-        int l1 =  Integer.MIN_VALUE;
-        int l2 = Integer.MIN_VALUE;
-        int r1 = Integer.MAX_VALUE;
-        int r2 = Integer.MAX_VALUE;
-           if (mid > 0) l1 = a[mid - 1];
-            if (mid2 > 0) l2 = b[mid2 - 1];
-            if (mid < n1) r1 = a[mid];
-            if (mid2 < n2) r2 = b[mid2];
-            if(l1<=r2 && l2<=r1){
-          return Math.max(l1,l2);
-          
-        } else if( l1>r2){
-          e =mid-1;
-        } else {
-          s=mid+1;
+            int cut1 = s+(e-s)/2;
+            int cut2 = k-cut1;
+            int l1 = (cut1==0) ? Integer.MIN_VALUE : nums1[cut1-1];
+            int l2 = (cut2==0) ? Integer.MIN_VALUE : nums2[cut2-1];
+            int r1 = (cut1==n1) ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = (cut2==n2) ? Integer.MAX_VALUE : nums2[cut2];
+            if(l1<=r2&&l2<=r1){
+                return Math.max(l1,l2);
+            }
+            if(l1>r2){
+                e=cut1-1;
+            }else{
+                s=cut1+1;
+            }
         }
-     }
-      return 0;
-        }
+
+        return 0;
+        
+    }
 }
