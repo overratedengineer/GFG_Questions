a22d9c7946abee280102b04fcaea12127387b2f5
Pulkit <choprapulkitt@gmail.com>
2026-07-24 19:28:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.java
@@ -1,29 +1,30 @@
 class Solution {
     public int kthElement(int nums1[], int nums2[], int k) {
-       if(nums1.length > nums2.length)
-            return kthElement(nums2, nums1,k);
-        int n1 = nums1.length;
-        int n2 = nums2.length;
-       int s = Math.max(0, k - n2);
-       int e = Math.min(k, n1);
-        while(s<=e){
-            int cut1 = s+(e-s)/2;
-            int cut2 = k-cut1;
-            int l1 = (cut1==0) ? Integer.MIN_VALUE : nums1[cut1-1];
-            int l2 = (cut2==0) ? Integer.MIN_VALUE : nums2[cut2-1];
-            int r1 = (cut1==n1) ? Integer.MAX_VALUE : nums1[cut1];
-            int r2 = (cut2==n2) ? Integer.MAX_VALUE : nums2[cut2];
-            if(l1<=r2&&l2<=r1){
-                return Math.max(l1,l2);
-            }
-            if(l1>r2){
-                e=cut1-1;
-            }else{
-                s=cut1+1;
-            }
-        }
-
-        return 0;
-        
+         // kth index 
+          int n = nums1.length;
+          int m = nums2.length;
+          if(n>m) kthElement(nums2,nums1,k);
+          int s = Math.max(0,k-m);
+          int e = Math.min(n,k);
+          while(s<=e){
+              int mid = s+(e-s)/2;
+              int need = k-mid;
+              int l1 = (mid>0)?nums1[mid-1]:Integer.MIN_VALUE;
+              int r1 = (mid<n)?nums1[mid]:Integer.MAX_VALUE;
+              int l2 = (need>0)?nums2[need-1]:Integer.MIN_VALUE;
+              int r2 = (need<m)?nums2[need]:Integer.MAX_VALUE;
+              if(l1<=r2 && r1>=l2){
+                  return Math.max(l2,l1);
+              } else if(l1>r2){
+                  e=mid-1;
+              } else {
+                  s=mid+1;
+              }
+              
+          }
+          return 0;
     }
 }
+   // 2 3 6 - 7 9
+   // 1 4   - 8 10 
+  
