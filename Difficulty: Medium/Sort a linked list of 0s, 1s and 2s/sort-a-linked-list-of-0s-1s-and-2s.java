19ea62777a61aeb006fd6e3f0c39c3d0529ae71e
Pulkit <choprapulkitt@gmail.com>
2026-07-27 22:40:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -1,44 +1,50 @@
+/*
+class Node {
+    int data;
+    Node next;
 
-// class Node {
-//   int val;
-//     Node next;
-
-//   Node(int d)
-//     {
-//         val = d;
-//       next = null;
-//   }
-// }
+    Node(int d)
+    {
+        data = d;
+        next = null;
+    }
+}*/
 
 class Solution {
     public Node segregate(Node head) {
-         int count0 = 0, count1 = 0, count2 = 0;
-        Node curr = head;
-
-        
-        while (curr != null) {
-            if (curr.data == 0) count0++;
-            else if (curr.data == 1) count1++;
-            else count2++;
-            curr = curr.next;
+        Node zero = new Node(-1);
+        Node ones = new Node(-1);
+        Node twos = new Node(-1);
+        Node temp1=zero;
+        Node temp2=ones;
+        Node temp3=twos;
+        Node temp = head;
+        Node curr=temp;
+        while(curr!=null)
+        {
+            temp=curr;
+            if(temp.data==0) {
+                temp1.next=temp;
+                temp1=temp1.next;
+            }
+            if(temp.data==1) {
+                temp2.next=temp;
+                 temp2=temp2.next;
+            }
+            if(temp.data==2) {
+                temp3.next=temp;
+                 temp3=temp3.next;
+            }
+             curr=temp.next;
+            temp.next=null;
         }
-
-       
-        curr = head;
-        while (count0-- > 0) {
-            curr.data = 0;
-            curr = curr.next;
+        temp1.next=(ones.next!=null)?ones.next:twos.next;
+        if(ones.next!=null){
+        temp2.next = twos.next;
         }
-        while (count1-- > 0) {
-            curr.data = 1;
-            curr = curr.next;
-        }
-        while (count2-- > 0) {
-            curr.data = 2;
-            curr = curr.next;
-        }
-
-        return head;
+       if (zero.next != null) return zero.next;
+       if (ones.next != null) return ones.next;
+       return twos.next;
         
     }
 }
